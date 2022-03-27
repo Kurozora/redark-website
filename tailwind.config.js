b447810edad57694bafdaea37b6afeeaaff2250a
musa11971 <mussesemou99@gmail.com>
2022-03-27 19:24:03 +0200

    Added initial source

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'redark-red': {
+          50: '#dea4b4',
+          100: '#c98799',
+          200: '#b36f81',
+          300: '#a15d6f',
+          400: '#a6556a',
+          500: '#a84761',
+          600: '#ab3a59',
+          700: '#ad284d',
+          800: '#b01a44',
+          900: '#b60033'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
